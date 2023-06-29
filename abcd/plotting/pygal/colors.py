661134fefc6332aeb915fff2da824312b6fbd0e8
camgbus <camilag.bustillo@gmail.com>
2023-06-29 16:57:05 -0700

    More visualizations

--- abcd/plotting/pygal/colors.py
+++ abcd/plotting/pygal/colors.py
@@ -4,7 +4,7 @@
 """Color mappings for different variables
 """
 
-palette = {'mint': '#9AE2E0', 'light_blue': '#6AB8E8', 'purple': '#A99EEC', 'pink': '#FE8AA8', 'orange': '#FFA581', 'yellow': '#F1DD65', 'green': '#ACDD7F'}
+palette = {'mint': '#9AE2E0', 'light_blue': '#6AB8E8', 'purple': '#A99EEC', 'pink': '#FE8AA8', 'orange': '#FFB293', 'yellow': '#F1DD65', 'green': '#ACDD7F'}
 
 CAT_COLORS = dict()
 CAT_COLORS['sex'] = {'F': palette['pink'], 'M': palette['light_blue']}
