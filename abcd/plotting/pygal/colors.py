16e2d5fbeb2d2719d286a2c9d42dfcd870ce7647
camgbus <camilag.bustillo@gmail.com>
2023-06-29 15:04:41 -0700

    Visualize subjects per split

--- abcd/plotting/pygal/colors.py
+++ abcd/plotting/pygal/colors.py
@@ -1,8 +1,11 @@
 """Color mappings for different variables
 """
 
-palette = {'mint': '#9AE2E0', 'celeste': '#6AB8E8', 'purple': '#A99EEC', 'pink': '#FE8AA8', 'yellow': '#FE8AA8'}
+"""Color mappings for different variables
+"""
+
+palette = {'mint': '#9AE2E0', 'light_blue': '#6AB8E8', 'purple': '#A99EEC', 'pink': '#FE8AA8', 'orange': '#FFA581', 'yellow': '#F1DD65', 'green': '#ACDD7F'}
 
 CAT_COLORS = dict()
 CAT_COLORS['sex'] = {'F': palette['pink'], 'M': palette['light_blue']}
-CAT_COLORS['split'] = {'0': palette['mint'], '1': palette['celeste'], '2': palette['purple'], '3': palette['pink'], '4': palette['yellow']}
+CAT_COLORS['split'] = {'0': palette['mint'], '1': palette['light_blue'], '2': palette['purple'], '3': palette['pink'], '4': palette['orange']}
