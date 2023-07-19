a73970c5d2d49fd60bd0adac9508517022e31aaf
camgbus <camilag.bustillo@gmail.com>
2023-07-19 12:17:29 -0700

    Visualization of mental health trajectories

--- abcd/plotting/seaborn/scatter.py
+++ abcd/plotting/seaborn/scatter.py
@@ -7,8 +7,18 @@ def plot_scatter(df, x_col, y_col, hue_col, title, figsize=(5, 5)):
     plt.figure(figsize=figsize)
     sns.scatterplot(x=x_col,
                     y=y_col, 
-                    hue=hue_col, palette=sns.color_palette("mako_r", as_cmap=True),
+                    hue=hue_col, #palette=sns.color_palette("mako_r", as_cmap=True),
                     data=df)
     plt.title(title, weight='bold')
     plt.legend(title=hue_col, loc='upper right', bbox_to_anchor=(1.45, 1.01))
+    return plt
+
+def plot_jointplot(df, x_col, y_col, hue_col, title, figsize=(5, 5)):
+    plt.figure(figsize=figsize)
+    sns.jointplot(x=x_col,
+                    y=y_col, 
+                    hue=hue_col, #palette=sns.color_palette("mako_r", as_cmap=True),
+                    data=df)
+    #plt.title(title, weight='bold')
+    plt.legend(title=hue_col, loc='upper right', bbox_to_anchor=(1.5, 1.5))
     return plt
