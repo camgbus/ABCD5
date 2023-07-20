e416b79238a32cd690aa16174cd750a3f88b202d
camgbus <camilag.bustillo@gmail.com>
2023-07-19 17:03:50 -0700

    More general residualization

--- abcd/data/var_tailoring/residualization.py
+++ abcd/data/var_tailoring/residualization.py
@@ -3,31 +3,37 @@
 import numpy as np
 import statsmodels.api as sm
 
-def residualize_age_sex(df, var, new_var_name=None, age_var="interview_age", 
-                        sex_var="kbi_sex_assigned_at_birth"):
-    
+def residualize(df, var, new_var_name=None, covs = ["interview_age", "kbi_sex_assigned_at_birth"],
+                references = {"kbi_sex_assigned_at_birth": 1.0}, verbose=False):
+                
     if not new_var_name:
         new_var_name = var
-        
-    mean_age = np.mean(df[age_var])
-    reference_sex = 1.0 # 1.0 is ,, 2.0 is f
-        
-    endog = df[var]  # endog: The variable we want to model
+    
+    # If a reference is not given, replace by the mean
+    for cov in covs:
+        if cov not in references:
+            references[cov] = np.mean(df[cov])
+            
+    # Endog: The variable we want to model
+    endog = df[var] 
     
     # Intercept/constant: the mean value of the response variable when all predictor variables 
     # in the model are equal to zero
     df["Intercept"] = 1
     
-    exog = df[["Intercept", sex_var, age_var]] # exog:  the covariates alias explanatory variables
+    # Exog:  the covariates alias explanatory variables
+    exog = df[["Intercept"] + covs] 
     
     # Generalized linear model
     md = sm.GLM(endog, exog, family=sm.families.Gaussian())
 
     # Fit the regression model
     md = md.fit()
-    print(md.summary())
+    if verbose:
+        print(md.summary())
 
-    # Calculate residual variables that end in 'prime'
-    df[new_var_name] = df[var] - (md.params[1] * (df[sex_var] - reference_sex) + md.params[2] * (df[age_var] - mean_age))
+    # Calculate residual variables
+    diff = sum(md.params[cov_ix+1] * (df[cov] - references[cov]) for cov_ix, cov in enumerate(covs))
+    df[new_var_name] = df[var] - diff
     
     return df
