757c33713d16417a3b1454e16878efbe9dc69a5e
camgbus <camilag.bustillo@gmail.com>
2023-08-03 15:00:08 -0700

    Fixed bug in plotting

--- abcd/plotting/pygal/training_progress.py
+++ abcd/plotting/pygal/training_progress.py
@@ -9,7 +9,7 @@ def plot_progress(df, y_column):
             ds_names.remove(ds_name)
             ds_names.insert(ds_name, 0)
     for ds_name in ds_names:
-        plot.add(ds_name, df.loc[df['Dataset']==ds][y_column])
+        plot.add(ds_name, df.loc[df['Dataset']==ds_name][y_column])
     plot.x_title = "Epoch"
     plot.x_labels = sorted(list(set(df['Epoch'])))
     plot.y_title = y_column
